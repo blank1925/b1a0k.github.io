98b454a0030fe4fe36f0591dc46b260bbacd3f6a
blank1925 <b1a0k@blank.ren>
2020-04-19 22:18:23 +0800

    fix some

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ function flushTime() {
     time_view.innerHTML = left_time <= 0 ? time_view.parentElement.innerHTML = "End on now." : +left_time;
 }
 
-function flushProgress() {
-    var progress_view = document.getElementsByClassName("progress")[0];
-    if (!progress_view) return;
-    var remaining = 1 - ((end_time - getNow()) / (end_time - start_time));
+// function flushProgress() {
+//     var progress_view = document.getElementsByClassName("progress")[0];
+//     if (!progress_view) return;
+//     var remaining = 1 - ((end_time - getNow()) / (end_time - start_time));
 
-    progress_view.setAttribute("value", remaining);
-}
+//     progress_view.setAttribute("value", remaining);
+// }
 
 function getNow() {
     return new Date().getTime();
 }
 
-function flushProgressTip() {
-    var tip_view = document.getElementsByClassName("progress-value")[0];
+// function flushProgressTip() {
+//     var tip_view = document.getElementsByClassName("progress-value")[0];
 
-    var remaining = +document.getElementsByClassName("progress")[0]?.getAttribute("value");
+//     var remaining = +document.getElementsByClassName("progress")[0]?.getAttribute("value");
 
-    tip_view.innerHTML = remaining >= 1 ? 100 : (remaining * 100).toFixed(7);
-}
+//     tip_view.innerHTML = remaining >= 1 ? 100 : (remaining * 100).toFixed(7);
+// }
 
 var interval = setInterval(() => {
     flushTime();
-    flushProgress();
-    flushProgressTip();
+    // flushProgress();
+    // flushProgressTip();
 }, 17);
 
 setTimeout(()=>{if(end_time - getNow() <= 0) clearInterval(interval)},100);
